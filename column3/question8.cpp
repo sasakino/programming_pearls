d62aced8a939ce01a490b120ffe2ba576642f188
sasaki <sasaki@113970-F4443.nsl.ad.nec.co.jp>
2020-07-20 23:09:19 +0900

    add answers

--- column3/question8.cpp
+++ column3/question8.cpp
@@ -3,26 +3,49 @@
 #include <array>
 #include "../utils.h"
 
-std::vector<int> convert(const unsigned short num, std::map<unsigned char, unsigned char> segdict) {
+class SevenSegmentDisplay {
+public:
+  SevenSegmentDisplay() {
+    this->segdict[0] = 125;
+    this->segdict[1] =  80;
+    this->segdict[2] =  55;
+    this->segdict[3] =  87;
+    this->segdict[4] =  90;
+    this->segdict[5] =  79;
+    this->segdict[6] = 111;
+    this->segdict[7] =  92;
+    this->segdict[8] = 127;
+    this->segdict[9] =  94;
+  }
+
+  ~SevenSegmentDisplay(){}
+
+  std::vector<int> convert(const unsigned short num);
+
+  void show(std::vector<unsigned char> segdata);
+
+private:
+  std::map<unsigned char, unsigned char> segdict;
+};
+
+std::vector<int> SevenSegmentDisplay::convert(const unsigned short num) {
   std::vector<int> segdata(5);
 
-  unsigned short digit = 10000;
-  unsigned short idx = 0;
+  int d = 10000; 
+  int n = 0, a = 0, _a = 0;
 
   for(int i = 0; i < 5; i++) {
-    idx = num / digit - idx * digit;
-    std::cout << idx << std::endl;
-    int n = segdict[idx];
-
-    segdata[i] = n;
-
-    digit/=10;
+    a = a;
+    a = num / d;
+    n = a - _a * 10;
+    segdata[i] = segdict[n];
+    d = d / 10;
   }
 
   return segdata;
 }
 
-void show(std::vector<unsigned char> segdata) {
+void SevenSegmentDisplay::show(std::vector<unsigned char> segdata) {
   std::array<std::array<bool, 15>, 3> seg;
 
   return;
@@ -30,19 +53,9 @@ void show(std::vector<unsigned char> segdata) {
 
 int main() {
 
-  std::map<unsigned char, unsigned char> segdict;
-  segdict[0] = 125;
-  segdict[1] =  80;
-  segdict[2] =  55;
-  segdict[3] =  87;
-  segdict[4] =  90;
-  segdict[5] =  79;
-  segdict[6] = 111;
-  segdict[7] =  92;
-  segdict[8] = 127;
-  segdict[9] =  94;
+  SevenSegmentDisplay sd;
 
-  std::vector<int> a = convert(12345, segdict);
+  std::vector<int> a = sd.convert(12345);
 
   dump(a);
 
